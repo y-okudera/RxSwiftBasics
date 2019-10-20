fbeb340ce5453cd3fbb6659d9a5c919d17436698
Yuki Okudera <appledev.yuoku@gmail.com>
2019-10-21 01:11:37 +0900

    [Training1] RxSwiftでボタンタップイベントを登録、TextFieldに入力したテキストをラベルに反映

--- Training1/Training1/ViewController.swift
+++ Training1/Training1/ViewController.swift
@@ -6,12 +6,56 @@
 //  Copyright © 2019 Yuki Okudera. All rights reserved.
 //
 
+import RxCocoa
+import RxSwift
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet private weak var textField: UITextField!
+    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var resetCountButton: UIButton!
+    @IBOutlet private weak var countUpButton: UIButton!
+    
+    private let disposeBag = DisposeBag()
+    private var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupRx()
+    }
+}
+
+extension ViewController {
+    
+    private func setupRx() {
+        
+        // UITextFieldに入力したテキストをUILabelに反映させる
+        self.textField.rx.text.orEmpty.subscribe { [weak self] text in
+            guard let weakSelf = self else {
+                return
+            }
+            weakSelf.label.text = text.element
+        }
+        .disposed(by: self.disposeBag)
+        
+        // UIButtonのタップイベント
+        self.countUpButton.rx.tap.subscribe { [weak self] _ in
+            guard let weakSelf = self else {
+                return
+            }
+            weakSelf.count += 1
+            weakSelf.countUpButton.setTitle("Count is \(weakSelf.count)!!", for: .normal)
+        }
+        .disposed(by: self.disposeBag)
+        
+        self.resetCountButton.rx.tap.subscribe { [weak self] _ in
+            guard let weakSelf = self else {
+                return
+            }
+            weakSelf.count = 0
+            weakSelf.countUpButton.setTitle("Count up!", for: .normal)
+        }
+        .disposed(by: self.disposeBag)
     }
 }
